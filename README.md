54f6da1fdef433b64f4fa35e7e757dc370fa731a
Tomas Kala <me@tomaskala.com>
2021-10-02 00:48:05 +0200

    [wip] Add a sway config

--- README.md
+++ README.md
@@ -3,8 +3,29 @@
 My Wayland-based environment setup.
 
 
+## Environment
+
+```
+$ sudo pacman -S sway swaylock alacritty brightnessctl wl-clipboard
+```
+
+
 ## Pipewire
 
 ```
 $ sudo pacman -S pipewire pipewire-pulse
 ```
+
+
+## Dotfiles
+
+Add the following configuration to the following files.
+
+* `~/.bash_profile`
+  ```
+  export TERMINAL='alacritty'
+
+  if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+    exec sway
+  fi
+  ```
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eu
+
+
+GLOBIGNORE=".:..:.git:.gitignore"
+
+
+install_dotfile() {
+  dotfile="$1"
+  dest="${HOME}/${dotfile}"
+
+  printf 'Installing %s\n' "${dotfile}"
+  mkdir -p "$(dirname "${dest}")"
+
+  chmod go-rwx "${dotfile}"
+  ln -fs "$(pwd)/${dotfile}" "${dest}"
+}
+
+
+for dotfiles_source in .*; do
+  find "${dotfiles_source}" -type f | sort | while read -r dotfile; do
+    install_dotfile "${dotfile}"
+  done
+done
